6a4cc57bfdaf7a34b1f128632d05f0ba351c58cb
Xeyth <mihai.oltean.net@live.com>
2023-03-15 16:11:57 +0200

    Initial Setup

---
+++ InhouseCore.Web/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+
+using InhouseCore.Web.Server.Models;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace InhouseCore.Web.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
