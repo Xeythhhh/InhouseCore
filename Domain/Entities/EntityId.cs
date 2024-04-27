bc6e677b1918562e4ffb44da34f237f3a20f9260
Xeyth <mihai.oltean.net@live.com>
2024-04-27 23:41:40 +0300

    Add Explicit conversion for EntityId

--- Domain/Entities/EntityId.cs
+++ Domain/Entities/EntityId.cs
@@ -1,6 +1,8 @@
 ﻿namespace Domain.Entities;
-
-public abstract record EntityId<TEntityId>(Ulid Value) : IEntityId
+public abstract record EntityId<TEntityId>(Ulid Value = default) : IEntityId
 {
     public static implicit operator Ulid(EntityId<TEntityId> id) => id.Value;
+    public static explicit operator EntityId<TEntityId>(Ulid ulid) => (EntityId<TEntityId>)
+        (Activator.CreateInstance(typeof(TEntityId), new object[] { ulid })
+        ?? throw new InvalidCastException($"Can not convert {ulid} to {nameof(TEntityId)}"));
 }
