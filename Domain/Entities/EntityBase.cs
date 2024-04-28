ac30139f7a55d0692f02a3894d885431855e7a96
Xeyth <mihai.oltean.net@live.com>
2024-04-28 16:21:21 +0300

    Updated Entity

--- Domain/Entities/EntityBase.cs
+++ Domain/Entities/EntityBase.cs
@@ -4,7 +4,7 @@
 public abstract class EntityBase<TEntityId> : IEntity<TEntityId>
     where TEntityId : IEntityId
 {
-    private readonly List<DomainEvent> _domainEvents = new();
+    protected readonly List<DomainEvent> _domainEvents = new();
 
     public TEntityId Id { get; protected set; }
     public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
@@ -13,14 +13,10 @@ public abstract class EntityBase<TEntityId> : IEntity<TEntityId>
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     protected EntityBase() { }         // Used by EFCore 
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    protected EntityBase(TEntityId id)
-    {
-        Id = id;
-    }
 
-    public IEnumerable<DomainEvent> GetDomainEvents() => _domainEvents.ToList();
-    public void ClearDomainEvents() => _domainEvents.Clear();
-    public void RaiseEvent(DomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+    public virtual IEnumerable<DomainEvent> GetDomainEvents() => _domainEvents.ToList();
+    public virtual void ClearDomainEvents() => _domainEvents.Clear();
+    public virtual void RaiseEvent(DomainEvent domainEvent) => _domainEvents.Add(domainEvent);
 }
 
 public class DomainEvent
