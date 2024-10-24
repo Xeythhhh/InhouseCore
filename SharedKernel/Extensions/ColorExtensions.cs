6d1957a2912a863174b2e9293871f57128bab2a1
Xeyth <mihai.oltean.net@live.com>
2024-10-24 12:13:05 +0300

    Update xml documentation

--- SharedKernel/Extensions/ColorExtensions.cs
+++ SharedKernel/Extensions/ColorExtensions.cs
@@ -3,6 +3,8 @@
 namespace SharedKernel.Extensions;
 public static class ColorExtensions
 {
+    /// <summary>Converts color to string representation of the hex code.</summary>
+    /// <remarks>Ignores the alpha channel!</remarks>
     public static string ColorToHex(this Color color) =>
         $"#{color.R:X2}{color.G:X2}{color.B:X2}";
 }
